aa30e8b882e2b23505610d3b97f06fbd1bf614e8
Mikael Dunhem <mikael.dunhem@gmail.com>
2016-12-29 21:45:45 -0500

    Adds TableComponent to the table module'

--- src/app/widgets/table/table.module.ts
+++ src/app/widgets/table/table.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TablePartsModule } from './table-parts/table-parts.module';
+import { TableComponent } from './table.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TablePartsModule
   ],
-  declarations: []
+  declarations: [TableComponent],
+  exports: [
+    TableComponent
+  ]
 })
 export class TableModule { }
