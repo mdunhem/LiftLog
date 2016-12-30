12dfb96acd80b6a9c3c35290a3a1a9d366e8584a
Mikael Dunhem <mikael.dunhem@gmail.com>
2016-12-30 16:43:46 -0500

    Cleans up the tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,25 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
+import { RouterOutletStubComponent } from '../testing';
+
+
 describe('AppComponent', () => {
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        SharedModule
       ],
+      declarations: [
+        AppComponent,
+        RouterOutletStubComponent
+      ]
     });
     TestBed.compileComponents();
-  });
+  }));
 
   it('should create the app', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
@@ -19,16 +27,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+  // it(`should have as title 'app works!'`, async(() => {
+  //   let fixture = TestBed.createComponent(AppComponent);
+  //   let app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('app works!');
+  // }));
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  // it('should render title in a h1 tag', async(() => {
+  //   let fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   let compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  // }));
 });
--- src/app/shared/main-layout/main-layout.component.spec.ts
+++ src/app/shared/main-layout/main-layout.component.spec.ts
@@ -1,9 +1,12 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, Component } from '@angular/core';
 
 import { MainLayoutComponent } from './main-layout.component';
+
+@Component({selector: 'side-nav', template: ''})
+class SideNavStubComponent {}
 
 describe('MainLayoutComponent', () => {
   let component: MainLayoutComponent;
@@ -11,7 +14,7 @@ describe('MainLayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainLayoutComponent ]
+      declarations: [ MainLayoutComponent, SideNavStubComponent ]
     })
     .compileComponents();
   }));
