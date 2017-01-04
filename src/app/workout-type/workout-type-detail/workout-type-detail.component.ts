d1e44bb9570a378be0021ed6f6db63b9138f1ac2
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-01-04 09:38:22 -0500

    Adds reactive form to workout-type-detail component

--- src/app/workout-type/workout-type-detail/workout-type-detail.component.ts
+++ src/app/workout-type/workout-type-detail/workout-type-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
-import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/switchMap';
+
+import { WorkoutTypeLiftLogFirebaseDatabaseService, WorkoutType } from '../../shared/services';
 
 @Component({
   selector: 'app-workout-type-detail',
@@ -11,18 +13,31 @@ import 'rxjs/add/operator/switchMap';
 })
 export class WorkoutTypeDetailComponent implements OnInit {
 
+  form: FormGroup;
+
   @Input() key: string;
 
-  workoutType: Observable<any>;
-  exerciseDefaults: FirebaseListObservable<any[]>;
+  workoutType: Observable<WorkoutType>;
+  // exerciseDefaults: FirebaseListObservable<any[]>;
 
-  constructor(private firebase: AngularFireDatabase, private route: ActivatedRoute) { }
+  constructor(private formBuilder: FormBuilder, private firebase: WorkoutTypeLiftLogFirebaseDatabaseService, private route: ActivatedRoute) {
+    this.form = this.formBuilder.group({
+      name: '',
+      exerciseDefaults: ['']
+    });
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      this.workoutType = this.firebase.object('workoutTypes/' + params['key']);
+      this.workoutType = this.firebase.object(params['key']);
+      this.workoutType.subscribe(value => {
+        this.form.setValue({
+          name: value.name,
+          exerciseDefaults: value.exerciseDefaults
+        });
+      });
     });
-    this.exerciseDefaults = this.firebase.list('exerciseDefaults');
+    // this.exerciseDefaults = this.firebase.list('exerciseDefaults');
   }
 
 }
